b82a75bb7ef081048de1d1ed5a5311ec23c3f57f
冰鹿踏雪 <330261243@qq.com>
2022-05-26 21:49:34 +0800

    Css样式的多种处理方式

--- src/components/ClassComponent.vue
+++ src/components/ClassComponent.vue
@@ -1,13 +1,33 @@
 <template>
 <div class="ClassComponent">
-  <div class="c1">
+  <div class="c1 package">
     样式分离
+  </div>
+  <div :class="{ current: active }">
+    实现样式的动态计算
+  </div>
+  <div :class="[current]">
+    :class 可以使用数组的形式管理多个class样式
+  </div>
+  <div :class="[{ current: active }]">
+    :class 也可以是数组对象的形式
+  </div>
+  <div :class="classLists">
+   可以使用返回的数据
   </div>
 </div>
 </template>
 
 <script>
   export default {
+    data(){
+      return{
+        active:true,
+        classLists:{
+          current: true
+        }
+      }
+    }
     
   }
 </script>
@@ -17,9 +37,12 @@
 // scoped 使用css属性选择器的方法定义样式 可以有效地将组件像是隔离  会出现性能的开销
 .ClassComponent{
   .c1 {
-  color: red;
+  color: white;
   }
-
+  .current {
+    background-color:#000;
+    color: white;
+  }
   // 为样式添加父级也可有效的隔离class样式
 
 
