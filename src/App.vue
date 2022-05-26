7dec0b69782ebb5723c6c515322abb5ddcb4f0af
冰鹿踏雪 <330261243@qq.com>
2022-05-26 21:31:54 +0800

    组件样式隔离的几种方式

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<ClassComponent/>
+<!-- <class-component/> -->
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<script>
+import ClassComponent from './components/ClassComponent.vue'
+  export default {
+    components: { ClassComponent },
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
 </style>
---
+++ src/components/ClassComponent.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="ClassComponent">
+  <div class="c1">
+    样式分离
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+// scoped 使用css属性选择器的方法定义样式 可以有效地将组件像是隔离  会出现性能的开销
+.ClassComponent{
+  .c1 {
+  color: red;
+  }
+
+  // 为样式添加父级也可有效的隔离class样式
+
+
+
+
+}
+</style>
