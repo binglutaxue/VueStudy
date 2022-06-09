0c4c6286ac64116194f5457cf3aa3b8f35e7ca1d
冰鹿踏雪 <330261243@qq.com>
2022-06-09 22:52:47 +0800

    完成按价格排序

--- src/components/LessonFor.vue
+++ src/components/LessonFor.vue
@@ -5,7 +5,12 @@
     <button @click=" orderBy = 'price'">价格排序</button>
     <button @click=" orderBy = 'comments'">评论数排序</button>
 
-    {{orderBy}}
+    <!-- {{orderBy}} -->
+    <!-- 升序降序 -->
+    
+    <button @click="lessonLists = 'asc'"> 升序</button>
+    <button @click="lessonLists = 'desc'">降序</button>
+
     <template v-for="leson in lessonLists" :key="leson.id">
       <div>{{leson.title}}-价格:{{ leson.price }} - 评论数： {{ leson.comments }}</div>
     </template>
@@ -18,19 +23,25 @@ import lessons from '../../data/lessons'
     data(){
       return {
         lessons,
-        orderBy: 'price'
+        orderBy: 'price',
         // 默认价格排序
-
+        orderType: 'asc'
       }
     },
     computed: { 
-       lessonLists() {
+
+       lessonLists: {
         //  console.log(this.lessons);
         //  console.log(this.lessons);
-          return this.lessons.sort((a,b)=>{
-             return a[this.orderBy] - b[this.orderBy]
+         get(){
+            return this.lessons.sort((a,b)=>{
+             return this.orderType == 'asc'? a[this.orderBy] - b[this.orderBy]:b[this.orderBy]-a[this.orderBy]
 
           })
+         },
+         set(type) {
+            this.orderType = type
+         }
        }
     }
   }
